0e38de655dedbfc4389375a41aa56db15c69ff37
Stoyan Dimov <s.dimov@codelogic.eu>
2016-09-27 21:04:44 +0200

    Add log4net ILogger adapter

---
+++ src/Scho.NET.Logging.Log4Net/LoggerFactoryAdapter.cs
@@ -0,0 +1,25 @@
+﻿using log4net;
+using log4net.Config;
+using Scho.NET.Logging.Contracts;
+
+namespace Scho.NET.Logging.Log4Net
+{
+	public class LoggerFactoryAdapter : ILoggerFactory
+	{
+
+		public ILogger CreateLogger(string name)
+		{
+			if (!LogManager.GetRepository().Configured)
+				XmlConfigurator.Configure();
+
+			ILog log = LogManager.GetLogger(name);
+			ILogger logger = new LoggerAdapter(log);
+			return logger;
+		}
+
+		public ILogger CreateLogger<T>()
+		{
+			return CreateLogger(typeof(T).FullName);
+		}
+	}
+}
